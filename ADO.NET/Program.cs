40fe0a057b73862d428d9e0ebc596f196cb3b38a
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-16 23:15:37 +0600

    ADO.net ,DataBase Connection

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,9 @@
+﻿using ADO.NET;
+
+var sql = "insert into student ([Name] , Cgpa , InActive , DateOfBirth) values('Hasan' , 3.44 , 0 , '2002-01-04')";
+
+
+SqlUtility sqlUtility = new SqlUtility();
+sqlUtility.ExcuteSql(sql);
+
+Console.WriteLine("Complete");
---
+++ ADO.NET/SqlUtility.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.NET
+{
+    public class SqlUtility
+    {
+        public void ExcuteSql(string sql)
+        {
+            var connectionString = "Server=Fahad\\SQLEXPRESS;Database=CSharp;User Id = csharpb22 ; password = 123456 ; Trust Server Certificate = True;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            int rowAffected = command.ExecuteNonQuery();//non query (write, update ,delete)
+
+
+            connection.Close();
+        }
+    }
+}
