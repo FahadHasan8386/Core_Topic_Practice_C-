5aab1d40ae66848cf4af7e85dd1e57fc760dfa52
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-25 11:31:20 +0600

    randdom file generating

---
+++ Assignment3/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static Random random = new Random(DateTime.Now.Millisecond);
+
+    static void Main(string[] args)
+    {
+        ///total byte 
+        long totalBytes = 5L * 1024 * 1024 * 1024;
+
+      
+        string current_Directory = Directory.GetCurrentDirectory();
+
+        string solution_Directory = Directory.GetParent(current_Directory).Parent.Parent.Parent.FullName;
+
+        string filePath = Path.Combine(solution_Directory, "LargeRandomFile.txt");
+
+     
+        Console.WriteLine("Generating file...");
+
+        ///Byte by byte write for 5 GB
+        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+        {
+            for (long i = 0; i < totalBytes; i++)
+            {
+                byte b = (byte)GetRandomChar();
+                fs.WriteByte(b);
+            }
+        }
+
+        Console.WriteLine("Completed generating.");
+    }
+
+    static char GetRandomChar()
+    {
+        return (char)random.Next('A', 'Z');
+    }
+}
