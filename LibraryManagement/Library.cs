12350041ded2d4301022d7d81076df7e893fa6ee
Fahad Hasan <hasan.fahad67@outlook.com>
2026-02-21 13:58:56 +0600

    Interview Preparetion on C#

---
+++ LibraryManagement/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement
+{
+    public class Library
+    {
+        public const string LibraryName = "City Library"; //Compile time constant
+        public readonly DateTime CreatedDate; // RunTime Constant
+
+        public Library() : this(DateTime.Now) { }
+
+        public Library(DateTime createdDate)
+        {
+            {
+                this.CreatedDate = createdDate;
+            }
+        }
+    }
+}
